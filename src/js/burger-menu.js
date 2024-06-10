e38d9f5fe7e96098430d62c20632bdc31b5034da
pain4metoo <pain4metoo@gmail.com>
2024-06-10 18:22:46 +0300

    feat: add adaptive for 320px

--- src/js/burger-menu.js
+++ src/js/burger-menu.js
@@ -1,17 +1,27 @@
 (function toggleBurgerMenu() {
-  let isShowBurger = false;
+  let isShowMenu = false;
+
   const burger = document.querySelector('.header_burger');
+  burger.onclick = () => handleOnClick();
+
   const burgetSticks = document.querySelectorAll('.header_burger_item');
   const headerBtnMob = document.querySelector('.header_btn_mob');
   const headerBtnLang = document.querySelector('.header_btn_lang');
 
-  burger.onclick = () => handleOnClick();
+  const headerNavItems = document.querySelectorAll('.header_nav_item');
+  Array.from(headerNavItems).map((el) => {
+    el.onclick = () => handleOnClick();
+  });
 
   const headerInner = document.querySelector('.header_inner');
+  const blackout = document.querySelector('.bg');
 
   function handleOnClick() {
     const windowInnerWidth = window.innerWidth;
 
+    isShowMenu = !isShowMenu;
+
+    blackout.classList.toggle('bg_active');
     headerInner.classList.toggle('header_nav_show');
     burger.classList.toggle('header_burger_fix');
     headerBtnMob.classList.toggle('header_btn_mob_toggle');
@@ -35,6 +45,13 @@
       headerBtnLang.style.display = 'none';
     } else {
       headerBtnLang.style.display = 'block';
+    }
+    if (windowInnerWidth > 900) {
+      blackout.classList.remove('bg_active');
+    } else {
+      if (isShowMenu) {
+        blackout.classList.add('bg_active');
+      }
     }
   };
 })();
